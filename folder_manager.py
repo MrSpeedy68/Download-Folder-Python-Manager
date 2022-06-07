4e4926cb33c3a18eeac6620a015b15ae6cdd5413
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2022-06-07 20:53:52 +0100

    Moving of files using shutil

--- folder_manager.py
+++ folder_manager.py
@@ -1,21 +1,24 @@
 import os
 import time
 import logging
+import fnmatch
+import shutil
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
-source_dest = "/Users/mrspe/Downloads"
-dest_dir_image = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Images"
-dest_dir_video = "/Users\mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Videos"
-dest_dir_3dprint = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded STLs"
-dest_dir_document = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Documents"
-dest_dir_music = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Music"
+source_dest = "/Users/mrspe/Downloads/"
+dest_dir_image = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Images/"
+dest_dir_video = "/Users\mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Videos/"
+dest_dir_3dprint = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded STLs/"
+dest_dir_document = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Documents/"
+dest_dir_music = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Music/"
 
 with os.scandir(source_dest) as entries:
     for entry in entries:
         if entry.is_file:
-            if ".pdf" in entry.name: 
+            if fnmatch.fnmatch(entry, '*.pdf'): 
                 print(entry.name)
+                shutil.move(source_dest + entry.name, dest_dir_document + entry.name)
 
 # if __name__ == "__main__":
 #     logging.basicConfig(level=logging.INFO,

7c1f0b88fd33cf7577e872fc1d2972d6114f13fd
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2022-06-07 23:15:59 +0100

    Observer reacts to events and files get moved over during event

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Download-Folder-Python-Manager
  A script to monitor and manage your downloads folder and to place all newly downloaded files into sub folders to sort files by file types like documents, images, videos etc.
+
+ ### Pip Installs required
+ - shutil
+ - watchdog
+ - logging
+ - shortuuid
+
+ ``` python -m pip install *pip library*```
+
+
--- folder_manager.py
+++ folder_manager.py
@@ -3,35 +3,67 @@ import time
 import logging
 import fnmatch
 import shutil
+import shortuuid
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-source_dest = "/Users/mrspe/Downloads/"
 dest_dir_image = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Images/"
 dest_dir_video = "/Users\mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Videos/"
 dest_dir_3dprint = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded STLs/"
 dest_dir_document = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Documents/"
 dest_dir_music = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Music/"
 
-with os.scandir(source_dest) as entries:
-    for entry in entries:
-        if entry.is_file:
-            if fnmatch.fnmatch(entry, '*.pdf'): 
-                print(entry.name)
-                shutil.move(source_dest + entry.name, dest_dir_document + entry.name)
+def move(src, dest, name):
+    file_exists = os.path.exists(dest + name) #Make sure the file name doesnt already exist in destination folder
+    if not file_exists:
+        shutil.move(src + name, dest + name)
+    else:
+        new_name = shortuuid.uuid() + name #Create random UUID to add infront of the name then move the file
+        os.rename(src + name, src + new_name)
+        shutil.move(src + new_name, dest + new_name)
 
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.INFO,
-#                         format='%(asctime)s - %(message)s',
-#                         datefmt='%Y-%m-%d %H:%M:%S')
-#     path = source_dest
-#     event_handler = LoggingEventHandler()
-#     observer = Observer()
-#     observer.schedule(event_handler, path, recursive=True)
-#     observer.start()
-#     try:
-#         while True:
-#             time.sleep(1)
-#     except KeyboardInterrupt:
-#         observer.stop()
-#     observer.join()
+def match_file(source_dest):
+    with os.scandir(source_dest) as entries:
+        for entry in entries:
+            if entry.is_file:
+                print(entry.name)
+                if fnmatch.fnmatch(entry, '*.pdf'): #Check for Documents
+                    move(source_dest, dest_dir_document,entry.name)
+
+
+class OnMyWatch:
+
+    source_dest = "/Users/mrspe/Downloads/"
+
+    def __init__(self):
+        self.observer = Observer()
+    
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.source_dest, recursive = True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+
+        self.observer.join()
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+        
+        elif event.event_type == 'created':
+            match_file(OnMyWatch.source_dest)
+        elif event.event_type == 'modified':
+            print("Something Modified")
+                        
+
+if __name__ == "__main__":
+    watch = OnMyWatch()
+    watch.run()

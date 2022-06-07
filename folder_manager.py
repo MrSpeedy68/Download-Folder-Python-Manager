384f2c80152ab2aee8d5dc42c6155ddce19cde6f
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2022-06-08 00:06:13 +0100

    Added all common extensions for images, videos, printing files, documents and music

--- folder_manager.py
+++ folder_manager.py
@@ -13,6 +13,12 @@ dest_dir_3dprint = "/Users/mrspe/OneDrive - Waterford Institute of Technology/De
 dest_dir_document = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Documents/"
 dest_dir_music = "/Users/mrspe/OneDrive - Waterford Institute of Technology/Desktop/Downloaded Music/"
 
+image_extension = ('.jpg', '.png', '.gif', '.webp', '.tiff', '.tif', '.psd', '.raw', '.bmp', '.heif', '.indd', 'jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.jp2', '.svg', '.svgz', '.ai', '.eps')
+video_extensions = ('.mp4', '.mov', '.wmv', '.avi', '.avichd', '.flv', '.flv4', '.swf', '.mkv', '.webm')
+print3d_extensions = ('.stl', '.obj', '.gcode', '.vrml', '.3mf', '.x3g', '.amf')
+document_extension = ('.pdf', '.doc', 'docx', '.txt', '.odt', '.xls', '.xlsx', '.ods', '.ppt', '.pptx')
+music_extension = ('.mp3', '.acc', '.ogg', '.flac', 'alac', '.wav', '.aiff', '.dsd', '.pcm')
+
 def move(src, dest, name):
     file_exists = os.path.exists(dest + name) #Make sure the file name doesnt already exist in destination folder
     if not file_exists:
@@ -26,9 +32,18 @@ def match_file(source_dest):
     with os.scandir(source_dest) as entries:
         for entry in entries:
             if entry.is_file:
-                print(entry.name)
-                if fnmatch.fnmatch(entry, '*.pdf'): #Check for Documents
+                filename = entry.name
+                if filename.endswith(image_extension): #Check for Image
+                    move(source_dest, dest_dir_image,entry.name)
+                elif filename.endswith(video_extensions): #Check for Videos
+                    move(source_dest, dest_dir_video,entry.name)
+                elif filename.endswith(document_extension): #Check for Documents
                     move(source_dest, dest_dir_document,entry.name)
+                elif filename.endswith(music_extension): #Check for Music
+                    move(source_dest, dest_dir_music,entry.name)
+                elif filename.endswith(print3d_extensions): #Check for 3DPrint Files
+                    move(source_dest, dest_dir_3dprint,entry.name)
+
 
 
 class OnMyWatch:
